e5a29235422dd3027683b7ecf79d4535fed803d8
Alex Winston <alex@piccana.co.uk>
2019-01-20 18:01:12 +0000

    Cleaned up JS and added functionality for infinite divs

--- script.js
+++ script.js
@@ -1,15 +1,12 @@
-color = c => {
-	r = Math.floor(Math.random() * 256);
-	g = Math.floor(Math.random() * 256);
-	b = Math.floor(Math.random() * 256);
-  
-    document.getElementById(`colorBox${c}`).style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
-  
-  console.log(`colorBox${c}`);
-}
+const rect = document.getElementsByClassName("three");
+const rectArr = Array.from(rect);
 
 runColor = () => {
-for(i = 1; i < 6; i++) {
-  color(i);
-}
+	rectArr.forEach(function(c){
+		r = Math.floor(Math.random() * 256);
+		g = Math.floor(Math.random() * 256);
+		b = Math.floor(Math.random() * 256);
+		c.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+		c.innerHTML = `<p>rgb(${r}, ${g}, ${b})</p>`;
+	});
 }

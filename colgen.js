34bdd02b622c07fd8eadd004c6ba22c75ea87a54
Alex Winston <alex@piccana.co.uk>
2019-01-23 23:57:59 +0000

    Changed layout, redesigned copy function, bug fixes, added RGB by default

--- colgen.js
+++ colgen.js
@@ -1,6 +1,63 @@
+// Constant Declaration and Set Up Functions
 const rect = document.querySelectorAll('.color-box');
 const colorDirectory = colors.colors;
-let colArray = [];
+let colArray = [
+	{
+		"name": "Bara Red",
+		"hex": "#e9546b",
+		"rgb": {
+			"r": 233,
+			"g": 84,
+			"b": 107
+		},
+		"luminance": 86.21810109831925,
+		"locked": false
+	},
+	{
+		"name": "Celadon Green",
+		"hex": "#2f847c",
+		"rgb": {
+			"r": 47,
+			"g": 132,
+			"b": 124
+		},
+		"luminance": 80.00677196962766,
+		"locked": false
+	},
+	{
+		"name": "Purple Thorn",
+		"hex": "#f0b9be",
+		"rgb": {
+			"r": 240,
+			"g": 185,
+			"b": 190
+		},
+		"luminance": 131.95274618210868,
+		"locked": false
+	},
+	{
+		"name": "Raw Cashew Nut",
+		"hex": "#c8beb1",
+		"rgb": {
+			"r": 200,
+			"g": 190,
+			"b": 177
+		},
+		"luminance": 128.14886883620937,
+		"locked": false
+	},
+	{
+		"name": "Lorna",
+		"hex": "#658477",
+		"rgb": {
+			"r": 101,
+			"g": 132,
+			"b": 119
+		},
+		"luminance": 84.26022912976204,
+		"locked": false
+	}
+];
 
 generateBoxID = rect => {
 	let i = 0;
@@ -9,23 +66,22 @@ generateBoxID = rect => {
 		i++;
 	});
 };
-
-init = (outArr, inArr) => {
-	let i = 0;
-
-	while(i < inArr.length) {
-		let randomHex = Math.floor(Math.random() * inArr.length);
-		if (!duplicateTest(outArr, inArr[randomHex])) {
-			outArr[i] = (inArr[randomHex]);
-			outArr[i].locked = false;
-			i++;
-		}
-	}
+createLockButton = () => {
+	const lockButtons = document.querySelectorAll(".lockBtn");
+	lockButtons.forEach(function(e) {
+		e.addEventListener("click", applyLock)
+	});
+};
+createCopyButton = () => {
+	const copyTarget = document.querySelectorAll(".copyBtn");
+	copyTarget.forEach(function (e) {
+		e.addEventListener("click", copyToClipboard);
+	});
 };
 
+// Color Generator
 runColor = () => {
 	colorGeneration(colArray, colorDirectory, rect.length);
-
 	rect.forEach(function(c){
 		if(!Boolean(c.className.toLowerCase().match('locked'))) {
 			c.style.backgroundColor = colArray[c.id].hex;
@@ -62,12 +118,9 @@ function duplicateTest(y, x) {
 
 colorDetails = (c) => {
 	let divObj = colArray[c.id];
-	c.children[0].children[0].innerText = divObj.name;
-	if(document.querySelector("#rgb-switch").classList == "hex") {
-		c.children[0].children[1].innerText = divObj.hex;
-	} else {
-		c.children[0].children[1].innerText = (`${divObj.rgb.r} ${divObj.rgb.g} ${divObj.rgb.b}`);
-	}
+	c.querySelector('.colorName').firstChild.textContent = divObj.name;
+	c.querySelector('#colorCodeHex').textContent = divObj.hex.toUpperCase();
+	c.querySelector('#colorCodeRGB').textContent = (`${divObj.rgb.r} ${divObj.rgb.g} ${divObj.rgb.b}`);
 };
 
 textContrast = (c) => {
@@ -84,12 +137,7 @@ textContrast = (c) => {
 	}
 };
 
-createLockButton = () => {
-	let lockButtons = document.querySelectorAll(".lock-btn");
-	lockButtons.forEach(function(e) {
-    e.addEventListener("click", applyLock)
-});
-};
+// Icon Functionality
 
 applyLock = event => {
 	event = event || window.event;
@@ -98,19 +146,18 @@ applyLock = event => {
 	colArray[tempDiv.id].locked = !colArray[tempDiv.id].locked;
 
 	if(colArray[tempDiv.id].locked) {
-		event.target.parentElement.innerHTML = '<i class="fas fa-lock lock-btn"></i>';
+		event.target.parentElement.innerHTML = '<i class="fas fa-lock"></i><span class="tooltiptext">Unlock this Colour</span>';
 	} else {
-		event.target.parentElement.innerHTML = '<i class="fas fa-unlock lock-btn"></i>';
+		event.target.parentElement.innerHTML = '<i class="fas fa-unlock"></i><span class="tooltiptext">Lock this Colour</span>';
 	}
-	createLockButton();
 };
 
 copyToClipboard = event => {
 	event = event || window.event;
- 	let tempA = document.body.appendChild(document.createElement('textarea'));
- 	let tempB = event.target.parentNode.children[0].children[1].textContent;
-	tempA.value = tempB.replace(/<\/?[a-zA-Z]+\/?>/g, '').trim();
-  	tempA.select();  
+ 	let tempTextArea = document.body.appendChild(document.createElement('textarea'));
+	let colorCodeContent = event.target.closest('.color-box').querySelector('.colorCode').firstChild.textContent;
+	tempTextArea.value = colorCodeContent.replace(/<\/?[a-zA-Z]+\/?>/g, '').trim();
+	tempTextArea.select();
   	document.execCommand("copy");
-  	tempA.remove();
+	tempTextArea.remove();
 };
--- general.js
+++ general.js
@@ -1,38 +1,13 @@
+// Set up
+generateBoxID(rect);
+runColor();
+createLockButton();
+createCopyButton();
+
+// Generate
+
 document.body.onkeyup = function(e) {
     if(e.keyCode == 32) {
         runColor();
     }
-};
-
-// set up
-generateBoxID(rect);
-init(colArray, rect);
-
-
-runColor();
-createLockButton();
-
-const copyTarget = document.querySelectorAll(".clickToCopy");
-	
-copyTarget.forEach(function(e) {
-    e.addEventListener("click", copyToClipboard);
-});
-
-swapCode = () => {
-    let x = document.querySelector("#rgb-switch");
-
-    if (x.classList == "hex") {
-        x.classList.add("rgb");
-        x.classList.remove("hex");
-        x.innerHTML = '<p class="rgb-switch" onclick="swapCode()"><i class="fas fa-exchange-alt" style="padding-right: 10px;"></i> Switch to HEX</p>';
-    } else {
-        x.classList.add("hex");
-        x.classList.remove("rgb");
-        x.innerHTML = '<p class="rgb-switch" onclick="swapCode()"><i class="fas fa-exchange-alt" style="padding-right: 10px;"></i> Switch to RGB</p>';
-    }
-
-    rect.forEach(function(c) {
-            let z = c.id;
-            colorDetails(c, z);
-        });
 };
